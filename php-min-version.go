a2105380ac37b63abfd7693cd2909bca250c4a4c
Shyim <s.sayakci@shopware.com>
2024-01-23 08:47:43 +0100

    Update php-min-version.go

--- php-min-version.go
+++ php-min-version.go
@@ -14,7 +14,9 @@ func generatePHPMinMap(ctx context.Context, tags []*github.RepositoryTag) error 
 	for _, tag := range tags {
 		tagName := strings.TrimPrefix(tag.GetName(), "v")
 
-		if strings.HasPrefix(tagName, "6.5") {
+		if strings.HasPrefix(tagName, "6.6") {
+			phpMinMap[tagName] = "8.2"
+		} else if strings.HasPrefix(tagName, "6.5") {
 			phpMinMap[tagName] = "8.1"
 		} else if strings.HasPrefix(tagName, "6.4") {
 			phpMinMap[tagName] = "7.4"
